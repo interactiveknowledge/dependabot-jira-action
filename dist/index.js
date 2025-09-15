f023c6fa6cdf19c4cf0dde54dfaa0e33322ff8d7
Rosemary Reilman <rosemary@interactiveknowledge.com>
2025-09-15 13:50:03 -0400

    Revert api changes

--- dist/index.js
+++ dist/index.js
@@ -602,7 +602,7 @@ function getJiraApiUrlV3(path = '/') {
 exports.getJiraApiUrlV3 = getJiraApiUrlV3;
 function getJiraSearchApiUrl() {
     const subdomain = process.env.JIRA_SUBDOMAIN;
-    const url = `https://${subdomain}.atlassian.net/rest/api/3/search/jql`;
+    const url = `https://${subdomain}.atlassian.net/rest/api/3/search`;
     return url;
 }
 exports.getJiraSearchApiUrl = getJiraSearchApiUrl;
